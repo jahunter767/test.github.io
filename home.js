4b23d5ae98654a6ff8ad5c4d391abe65ee28604b
Jason Hunter <jahunter767@gmail.com>
2022-01-04 13:00:31 -0500

    created separate html templates for nav bar and footer

---
+++ home.js
@@ -0,0 +1,10 @@
+"use strict";
+
+window.onload = function () {
+    let baseUrl = document.location;
+    console.log(document.location);
+    console.log(window.location.origin);
+    console.log(window.location.pathname);
+    console.log(window.location.href);
+    //fetch()
+}; // End-window.onload
