df251723b66a8f0010ce11270fd0442fda87b56a
Jason Hunter <jahunter767@gmail.com>
2022-01-04 17:46:55 -0500

    updated template, changed js to update a section of the page instead of generating a new page

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 "use strict";
 
-window.onload = async function () {
-    let body = document.getElementsByTagName("body")[0];
-
-    let navBar = await fetch("./nav-bar.html")
-        .then(res => res.text())
-        .then(res => res);
-
-    let footer = await fetch("./footer.html")
-        .then(res => res.text())
-        .then(res => res);
-
-    body.innerHTML = `${navBar}\n${body.innerHTML}\n${footer}`;
+async function loadPage(page){
+    let main = document.getElementById("main");
+    main.innerHTML = await fetch(page)
+        .then(res => {
+            for (let v of res.headers.keys()){
+                console.log(v);
+            }
+            console.log(res.headers.get("Last-Modified"));
+            return res.text();
+        }).then(res => res);
 
     let modDate = document.getElementById("last-modified");
     modDate.innerText = `Last Modified: ${document.lastModified}`;
+} // End-loadPage
+
+window.onload = async function () {
+    await loadPage("./home.html");
 }; // End-window.onload
